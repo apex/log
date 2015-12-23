97d7815d77a707125ef513a46a1cdd982d671218
TJ Holowaychuk <tj@vision-media.ca>
2015-12-23 14:48:52 -0800

    refactor Kinesis example

--- _examples/kinesis/kinesis.go
+++ _examples/kinesis/kinesis.go
@@ -11,12 +11,10 @@ import (
 )
 
 func main() {
-	handlers := multi.New(
+	log.SetHandler(multi.New(
 		text.New(os.Stderr),
 		kinesis.New("logs"),
-	)
-
-	log.SetHandler(handlers)
+	))
 
 	ctx := log.WithFields(log.Fields{
 		"file": "something.png",
