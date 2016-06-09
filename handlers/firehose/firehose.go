136f207ea7881713045bab67802f11521d721742
Erik Reppel <erikreppel@gmail.com>
2016-06-09 15:31:02 -0700

    NewConfig to return a handler based on the config params of a firehose + stream name

--- handlers/firehose/firehose.go
+++ handlers/firehose/firehose.go
@@ -1,18 +1,37 @@
 package firehose
 
 import (
-	"encoding/base64"
-	"encoding/json"
+	// "encoding/json"
 
-	"github.com/apex/log"
+	// "github.com/apex/log"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/client"
+	// "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
 	"github.com/rogpeppe/fastuuid"
 )
 
+// Handler implementation
 type Handler struct {
-	appName  string
-	producer *firehose.Firehose
-	gen      *fastuuid.Generator
+	appName    string
+	producer   *firehose.Firehose
+	gen        *fastuuid.Generator
+	streamName string
+}
+
+// New returns a handler for streaming logs into a firehose Kinesis stream.
+// Like the Kinesis handler, to configure producer options or pass our own AWS
+// Kinesis client use NewConfig instead
+func New(stream, region string) *Handler {
+
+}
+
+// NewConfig handler for streaming logs into a firehose Kinesis stream.
+// random value used as partition key
+func NewConfig(streamName string, c client.ConfigProvider, cfgs ...*aws.Config) *Handler {
+	fh := firehose.New(c, cfgs...)
+	return &Handler{
+		producer: fh,
+		gen:      fastuuid.MustNewGenerator(),
+	}
 }
