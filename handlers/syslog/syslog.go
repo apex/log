eefcf0e06c534337fcbcc2854f915e229bc899f8
Tessa Nordgren <tessa@sudo.ca>
2020-07-22 12:59:23 -0700

    updated to use logfmt

--- handlers/syslog/syslog.go
+++ handlers/syslog/syslog.go
@@ -2,67 +2,73 @@
 package syslog
 
 import (
-  "fmt"
-  "log/syslog"
+	"fmt"
+	"log/syslog"
 
-  "github.com/apex/log"
+	"github.com/apex/log"
+	"github.com/go-logfmt/logfmt"
 )
 
 // Handler implementation.
 type Handler struct {
-  log *syslog.Writer
-  // map apex levels to syslog levels.
-  levelsMap map[log.Level](func (m string) error)
+	log *syslog.Writer
+	// map apex levels to syslog levels.
+	levelsMap map[log.Level](func(m string) error)
 }
 
 // New syslog handler.
-//  addr: network address of syslog host. (default: localhost)type syslogOutput func (w *syslog.Writer)
+//  addr: network address of syslog host. (default: localhost)
 //  facility: syslog facility, as defined in "log/syslog". (default: LOG_USER)
 //  tag: message identifier, usually the application name. (default: os.Args[0])
 func New(addr string, facility syslog.Priority, tag string) (h *Handler, err error) {
-  h = new(Handler)
-  if facility == 0 {
-    facility = syslog.LOG_USER
-  }
+	h = new(Handler)
+	if facility == 0 {
+		facility = syslog.LOG_USER
+	}
 
-  if addr == "" {
-    if h.log, err = syslog.New(facility, tag); err != nil {
-      // sometimes local socket connections don't work, so try localhost before
-      // giving up.
-      if h.log, err = syslog.Dial("tcp", "localhost:514", facility, tag); err != nil {
-        return nil, err
-      }
-    }
-  } else {
-    if h.log, err = syslog.Dial("tcp", addr, facility, tag); err != nil {
-      return nil, err
-    }
-  }
+	if addr == "" {
+		if h.log, err = syslog.New(facility, tag); err != nil {
+			// sometimes local socket connections don't work, so try localhost before
+			// giving up.
+			if h.log, err = syslog.Dial("tcp", "localhost:514", facility, tag); err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		if h.log, err = syslog.Dial("tcp", addr, facility, tag); err != nil {
+			return nil, err
+		}
+	}
 
-  // create apex -> syslog level mapping dynamically with h.log methods.
-  // wish "log/syslog" had a Writer.Log(p Priority, m string) method.
-  h.levelsMap = map[log.Level](func (m string) error){
-    log.DebugLevel: h.log.Debug,
-    log.InfoLevel: h.log.Info,
-    log.WarnLevel: h.log.Warning,
-    log.ErrorLevel: h.log.Err,
-    log.FatalLevel: h.log.Emerg,
-  }
+	// create apex -> syslog level mapping dynamically with h.log methods.
+	// wish "log/syslog" had a Writer.Log(p Priority, m string) method.
+	h.levelsMap = map[log.Level](func(m string) error){
+		log.DebugLevel: h.log.Debug,
+		log.InfoLevel:  h.log.Info,
+		log.WarnLevel:  h.log.Warning,
+		log.ErrorLevel: h.log.Err,
+		log.FatalLevel: h.log.Emerg,
+	}
 
-  return h, nil
+	return h, nil
 }
 
 // HandleLog implements log.Handler.
 func (h *Handler) HandleLog(e *log.Entry) error {
-  out := e.Message + "  {"
+	keyvals := make([]interface{}, 0, len(e.Fields.Names()) * 2)
+	for _, name := range e.Fields.Names() {
+		if name == "source" {
+			continue
+		}
+		keyvals = append(keyvals, name)
+		keyvals = append(keyvals, e.Fields.Get(name))
+	}
+	fmt.Println("keyvals:", keyvals)
 
-  for _, name := range e.Fields.Names() {
-    if name == "source" {
-      continue
-    }
-    out += fmt.Sprintf(" %s=\"%v\"", name, e.Fields.Get(name))
-  }
-  out += " }"
-
-  return h.levelsMap[e.Level](out)
+	out, err := logfmt.MarshalKeyvals(keyvals...)
+	fmt.Println(string(out), err)
+	if err != nil {
+		return err
+	}
+	return h.levelsMap[e.Level](string(out))
 }
